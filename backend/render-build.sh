b6d7bd379bd050f3f0ebb26744e8f53b0eee5eb5
RaghavBansal1211 <raghavbansal1211@gmail.com>
2025-05-26 00:52:19 +0530

    changing paths

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -8,7 +8,7 @@ npm install
 # npm run build 
 
 # Ensure the Puppeteer cache directory exists
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+PUPPETEER_CACHE_DIR=/opt/render/project/src/backend/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 
 # Install Puppeteer and download Chrome
@@ -18,8 +18,8 @@ npx puppeteer browsers install chrome
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache"
   # Copying from the actual path where Puppeteer stores its Chrome binary
-  cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+  cp -R /opt/render/project/src/backend/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
 else 
   echo "...Storing Puppeteer Cache in Build Cache"
-  cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+  cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/backend/.cache/puppeteer/chrome/
 fi
